cb685d5996c58def785940ad543dbf5d6ce28d03
Humphrey Ikhalea <84229187+humphreydev6@users.noreply.github.com>
2023-09-05 08:12:38 +0100

    if statement

--- if_statement.py
+++ if_statement.py
@@ -2,10 +2,10 @@ x = int(input("Please enter an integer: "))
 #Please enter an integer: 42
 if x < 0:
     x = 0
-    print('Negative changed to zero')
+    print('The negative changes to zero')
 elif x == 0:
-    print('Zero')
+    print('This is a Zero value')
 elif x == 1:
-    print('Single')
+    print('This is a Single value')
 else:
-    print('More')
+    print('More value')
